bac73ef13647615f3569077a1b05f631ecca9d38
analuizaalvesm <analuizaalvesm@gmail.com>
2024-09-05 20:04:54 -0300

    feat: configuração inicial do Spring

---
+++ Implementacao/rent-car-system/src/main/java/com/example/rent_car_system/RentCarSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.rent_car_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentCarSystemApplication.class, args);
+	}
+
+}
---
+++ Implementacao/rent-car-system/src/test/java/com/example/rent_car_system/RentCarSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rent_car_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
